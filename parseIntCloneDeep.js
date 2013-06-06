100327670ce82250270125b82ec1fcf330f9f3ff
spencercarnage <spencercarnage@gmail.com>
2013-06-05 17:25:31 -0700

    finished basic parse int deep clone implentation

--- parseIntCloneDeep.js
+++ parseIntCloneDeep.js
@@ -4,34 +4,26 @@
  */
 
 _.mixin({
-  parseIntObj: function (obj, radix) {
+  parseIntDeepClone: function (obj, radix) {
     radix = radix || 10;
 
-    var returnObj = {};
-    var objClone = _.clone(obj);
-
-    var result = _.map(obj, function (value, key) {
-      console.log(value, key);
-
+    var convertedStr;
+    var objClone = _.cloneDeep(obj, function (value) {
       // If the value is not a String, append the original value to it and do
       // nothing else
       if (!_.isString(value)) {
-        returnObj[key] = value;
-        return value;
+        return undefined;
       }
 
-      var convertedStr = parseInt(value, radix);
+      convertedStr = parseInt(value, radix);
                                                                                   
       if (typeof convertedStr === 'number' && !_.isNaN(convertedStr)) {       
-        returnObj[key] = convertedStr;
         return convertedStr;
       } else {                                                                  
-        returnObj[key] = value;
-        return value;
+        return undefined;
       }                                                                         
     });
-    //return returnObj;
-    console.log(result);
-    return result;
+
+    return objClone;
   }
 });
--- tests/parseIntCloneDeep-tests.js
+++ tests/parseIntCloneDeep-tests.js
@@ -1,10 +1,17 @@
 test('Convert number strings into proper numbers', function () {
-  expect(6);
+  expect(7);
 
-  var originalObj = ({test: '1'});
-  var convertedObj = _.parseIntObj(originalObj);
-  var originalArr = ['1', '2', 3];
-  var convertedArr = _.parseIntObj(originalArr);
+  var originalObj = {
+    test: '1',
+    test2: {
+      test: '1',
+      test2: 1
+    }
+  };
+
+  var convertedObj = _.parseIntDeepClone(originalObj);
+  var originalArr = ['1', '2', 3, ['1']];
+  var convertedArr = _.parseIntDeepClone(originalArr);
 
   ok(
     typeof convertedObj.test === 'number', 
@@ -14,13 +21,13 @@ test('Convert number strings into proper numbers', function () {
   // Do a deepEqual for good measure
   deepEqual(
     convertedObj,
-    {test: 1}, 
+    {test: 1, test2: {test: 1, test2: 1}}, 
     "Should convert test string value into a number value."
   );
 
   deepEqual(
     originalObj,
-    {test: '1'},
+    {test: '1', test2: {test: '1', test2: 1}}, 
     "Should leave the original object untouched."
   );
 
@@ -29,16 +36,21 @@ test('Convert number strings into proper numbers', function () {
     "Should convert first index into a number"
   );
 
+  ok(
+    typeof convertedArr[3][0] === 'number', 
+    "Should convert first index into a number"
+  );
+
   // Do a deepEqual for good measure
   deepEqual(
     convertedArr,
-    [1,2,3],
+    [1,2,3, [1]],
     "Should convert all number string array indexes into a number value."
   );
 
   deepEqual(
     originalArr,
-    ['1', '2', 3],
+    ['1', '2', 3, ['1']],
     "Should leave the original array untouched."
   );
 });
