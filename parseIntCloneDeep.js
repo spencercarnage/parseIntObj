843b119dc9cd20b137354fb323314d48dbb236a2
spencercarnage <spencercarnage@gmail.com>
2013-06-05 17:30:57 -0700

    added minified copy of parseIntCloneDeep

--- parseIntCloneDeep.js
+++ parseIntCloneDeep.js
@@ -1,27 +1,24 @@
-/**
- * An Underscore.js mixin that does a shallow conversion of all number string
- * values into proper "Number" values.
- */
-
 _.mixin({
   parseIntDeepClone: function (obj, radix) {
     radix = radix || 10;
-
+    
     var convertedStr;
     var objClone = _.cloneDeep(obj, function (value) {
-      // If the value is not a String, append the original value to it and do
-      // nothing else
+
+      // If the value is not a String,append the original value to it and do 
+      // nothing else 
+      
       if (!_.isString(value)) {
         return undefined;
       }
-
+      
       convertedStr = parseInt(value, radix);
-                                                                                  
-      if (typeof convertedStr === 'number' && !_.isNaN(convertedStr)) {       
+      
+      if (typeof convertedStr === 'number' && !_.isNaN(convertedStr)){
         return convertedStr;
-      } else {                                                                  
+      } else {
         return undefined;
-      }                                                                         
+      }
     });
 
     return objClone;
